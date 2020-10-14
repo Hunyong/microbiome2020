4965b25d216f5898fca3435f210d06c863d44da8
Hunyong Cho <hunyongcho@Hunyongs-MacBook-Pro-2.local>
2020-10-14 12:33:46 -0400

    .

--- S01.gof.sh
+++ S01.gof.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 module load r/3.5.2
-for i in {1..2}; do # ZOE 1, 2
-  for j in {1..3}; do # tpm, rpk, asin
-    Rscript C01.01.goodness_of_fit.R $i $j;
-  done;
-done;
+Rscript C01.01.goodness_of_fit.R $1 $2;
+
+# for i in {1..2}; do # ZOE 1, 2
+#   for j in {1..3}; do # tpm, rpk, asin
+#     sbatch S01.gof.sh $i $j;
+#   done;
+# done;
