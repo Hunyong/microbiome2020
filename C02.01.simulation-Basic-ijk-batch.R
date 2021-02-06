92f0c44c7b3f23b671a6d01175a560ac12293166
Hunyong Cho <hunyongcho@Hunyongs-MacBook-Pro-2.local>
2021-02-06 10:25:15 -0500

    bookkeeping the fatal errors

--- C02.01.simulation-Basic-ijk-batch.R
+++ C02.01.simulation-Basic-ijk-batch.R
@@ -71,9 +71,9 @@ for (i in 1:10) {
     if (file.exists(save_file.stat)) {
       # bookkeeping
       if (file.exists(nm)) file.remove(nm)
-      nm = gsub("tmp_", "tmp_done_", nm)
-      nm = gsub("_s[0-9]*", "", nm)
-      write.table(" ", nm)
+      # nm = gsub("tmp_", "tmp_done_", nm)
+      # nm = gsub("_s[0-9]*", "", nm)
+      # write.table(" ", nm)
       
       next  # next i
       # stop("done already.")
@@ -351,9 +351,9 @@ for (i in 1:10) {
     
     # bookkeeping
     if (file.exists(nm)) file.remove(nm)
-    nm = gsub("tmp_", "tmp_done_", nm)
-    nm = gsub("_s[0-9]*", "", nm)
-    write.table(" ", nm)
+    # nm = gsub("tmp_", "tmp_done_", nm)
+    # nm = gsub("_s[0-9]*", "", nm)
+    # write.table(" ", nm)
     
     tt(2)
 }
