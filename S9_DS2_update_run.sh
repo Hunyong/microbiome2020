7fb8f5cddd79ab91e05226cc6c61542ffb37b19f
Hunyong Cho <hunyongcho@Hunyongs-MacBook-Pro-2.local>
2021-02-22 15:47:38 -0500

    (temporary files for DESeq2 results update)

---
+++ S9_DS2_update_run.sh
@@ -0,0 +1,21 @@
+# ZINB
+for l in {0..0}; do # ZINB, ZIG, ZILN
+  for j in {0..0}; do 
+    for k in {1..54}; do 
+      sbatch --time=10:00:00 --mem 10000 S9_DS2_update.sh 0 $j $k $l 5 80;
+      sbatch --time=40:00:00 --mem 10000  S9_DS2_update.sh 0 $j $k $l 5 400; 
+      if [[ "$l" = 3 ]]; then
+        sbatch --time=10:00:00 --mem 10000  2run-code.sh 0 $j $k $l 2.5 80;
+        sbatch --time=40:00:00 --mem 10000  2run-code.sh 0 $j $k $l 2.5 400;
+        sbatch --time=10:00:00 --mem 10000  2run-code.sh 0 $j $k $l 0 80;
+        sbatch --time=40:00:00 --mem 10000  2run-code.sh 0 $j $k $l 0 400;
+      fi;
+    done; 
+  done; 
+done;
+
+# ijk = scenarios
+# i is being looped inside Rscripts.
+# $4 = model (1 = zinb, 2 = zig, 3 = zlin)
+# $5 = perturb (5 = 50%)
+# $6 = n (80, 400)
