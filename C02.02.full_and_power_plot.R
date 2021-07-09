13f088e807cf69a5a362b270218032b376b3e642
Hunyong Cho <hunyongcho@Hunyongs-MacBook-Pro-2.local>
2021-07-09 00:01:37 -0400

    change in order (power / full)

--- C02.02.full_and_power_plot.R
+++ C02.02.full_and_power_plot.R
@@ -3,136 +3,6 @@ library(latex2exp)
 library(ggh4x) # for nested facetting
 source("C01.02.simulation.setup.R")
 
-fullplot <- function(size, model, res.tmp = TRUE, stop.if.absent = TRUE) # res.tmp being globally assigned
-{
-  
-  parameter = switch(model, 
-                     zinb = parameterNB, 
-                     zig = parameterLN, 
-                     ziln = parameterLN)
-  k.index = dim(parameter)[1]
-  param.k = apply(parameter[,-1], 1, function(x) paste0("(", paste(x, collapse=", "), ")"))
-  
-  
-  res <- NULL
-  for(i in 1:10)
-  {
-    
-    for(j in 1:5)
-    {
-      cat("\ni = ",i,"j = ",j, "k = ")
-      
-      for(k in 1:k.index)
-      {
-        cat(k , " ")
-        fn.tmp <- paste0("output/stat-n",size,"-pert0.5-",model,"-",i,".",j,".",k,".rds")
-        if (file.exists(fn.tmp)) {
-          result <- readRDS(fn.tmp)
-          result.stat <- data.frame(result$stat)
-          tmp <- 
-            result.stat %>% 
-            mutate ("LB" = LB.glob, "MAST" = MAST.glob, "KW-II" = Wg.glob,
-                    "i" = i,"j" = j,"k" = k,"batch_f" = as.character(result$setting$kappa[4]),
-                    "effect" = as.character(result$setting$delta[4])) %>%
-            dplyr::select(LB, LN, MAST, KW, `KW-II`, DS2, `DS2ZI`, MGS, 
-                          ANCOM.sz, ANCOM, i, j, k, batch_f, effect)
-          
-          res <- rbind(res,tmp[1,])
-          
-        } else {
-          if (stop.if.absent) stop("Not available")
-          cat("(Not available) ")
-        }
-      }
-    }
-  }
-  res <- res %>% gather(key = "method", value = "p.value",`LB`,`LN`,`MAST`,`KW`,
-                        `KW-II`,`DS2`, `DS2ZI`, `MGS`, `ANCOM.sz`, `ANCOM`)
-  
-  res$method_f = factor(res$method,
-                             levels = c("LN", "LB", "MAST", "DS2", "DS2ZI", "MGS", "ANCOM.sz", "ANCOM", "KW", "KW-II"),
-                             labels = c("LN", "LB", "MAST", "DS2", "DS2ZI", "MGS", "ANCOMsz", "ANCOM", "KW", "KW-II"))
-  res$effect_f = factor(res$effect,
-                             levels = c("Effect_null", "Effect_mu(D>H)", 
-                                        "Effect_theta(D>H)", "Effect_pi(D<H)",
-                                        "Effect_mu(D>H).theta(D>H)", "Effect_mu(D>H).pi(D<H)", 
-                                        "Effect_theta(D>H).pi(D<H)", "Effect_mu(D>H),pi(D>H)",
-                                        "Effect_mu(D>H).theta(D<H)", "Effect_theta(D<H).pi(D<H)"), 
-                             labels = c(TeX("D1 (null)"),
-                                        TeX("D2 ($\\mu_D$>$\\mu_{H}$)"), 
-                                        TeX("D3 ($\\theta_D$>$\\theta_{H}$)"),
-                                        TeX("D4 ($\\pi_D$<$\\pi_{H}$)"),
-                                        TeX("D5 ($\\mu_D$>$\\mu_H$, $\\theta_D$>$\\theta_{H}$)"),
-                                        TeX("D6 ($\\mu_D$>$\\mu_H$, $\\pi_D$<$\\pi_{H}$)"),
-                                        TeX("D7 ($\\theta_D$>$\\theta_H$, $\\pi_D$<$\\pi_{H}$)"),
-                                        TeX("D8 ($\\mu_D$>$\\mu_H$, $\\pi_D$>$\\pi_{H}$)"),
-                                        TeX("D9 ($\\mu_D$>$\\mu_H$, $\\theta_D$<$\\theta_{H}$)"),
-                                        TeX("D10 ($\\theta_D$<$\\theta_H$, $\\pi_D$<$\\pi_{H}$)")))
-  res$batch_f = factor(res$batch_f,
-                            levels = c("no batch effect", 
-                                       "small(+,+,-) batch effect",
-                                       "large(+,+,-) batch effect",
-                                       "large(+,-,-) batch effect",
-                                       "small(+,-,-) batch effect"),
-                            labels = c("K1 (0, 0, 0)",
-                                       "K2 (0.5, 0.5, -0.5)",
-                                       "K3 (1, 1, -1)",
-                                       "K4 (0.5, -0.5, -0.5)",
-                                       "K5 (1, -1, -1)"))
-# res.tmp <<- res
-  # res[res$method %in% c("MGS", "ANCOM", "ANCOM.sz") 
-  #     & res$j != 1, "p.value"] <- NA #NA for MGS, ANCOM, and ANCOM.sz with batch effects
-  res$k <- factor(res$k)
-  if (res.tmp) res.tmp <<- res
-  res %>%
-    ggplot(aes(k, p.value, fill = batch_f)) +
-    geom_bar(stat="identity", position = position_dodge(width = .8)) +
-    geom_hline(yintercept=0.05, col="black", linetype = 2) + ylim(c(0, 1)) + 
-    geom_point(aes(col = batch_f), position = position_dodge(width = .8), shape = 15, size = 0.1) +
-    # geom_text(data = res %>% filter(p.value==0), aes(label = p.value, col = batch_f), 
-    #           position = position_dodge(width = .8), size = 0.5) +
-    # geom_point(data = res %>% filter(p.value==0), aes(k, p.value, fill = batch_f, col = batch_f),
-    #            shape = 21, size = 0.2,
-    #            position = position_dodge()) +
-    guides(col = FALSE) + 
-    theme(legend.position = "none", axis.text.x = element_text(angle=90)) +
-    scale_x_discrete(labels=param.k,guide = guide_axis(n.dodge = 2)) +
-    scale_color_manual(values=c("K1 (0, 0, 0)"  = "dodgerblue",
-                               "K2 (0.5, 0.5, -0.5)"  = "darkgreen",
-                               "K3 (1, 1, -1)" = "chartreuse3",
-                               "K4 (0.5, -0.5, -0.5)"  = "red3",
-                               "K5 (1, -1, -1)" = "tomato1")) +
-    scale_fill_manual(name = TeX("Batch effects ($\\kappa_\\mu, \\kappa_\\theta, \\kappa_{\\pi}$)"),
-                      values=c("K1 (0, 0, 0)"  = "dodgerblue",
-                               "K2 (0.5, 0.5, -0.5)"  = "darkgreen",
-                               "K3 (1, 1, -1)" = "chartreuse3",
-                               "K4 (0.5, -0.5, -0.5)"  = "red3",
-                               "K5 (1, -1, -1)" = "tomato1")) +
-    xlab(expression("baseline (" * mu ~ ", " * theta * ", " * pi * ")")) +
-    ylab("rejection rate") +
-    facet_grid(rows = vars(method_f), cols = vars(effect_f), labeller = label_parsed) +
-    theme(plot.title = element_text(hjust = 0.5), legend.position="bottom")  -> p
-  
-  p
-}
-
-#width = 32, height = 16
-pn1 <- fullplot(80, model="ziln", stop.if.absent = FALSE)
-ggsave(file="figure/ziln_full_size80.pdf", pn1, width = 30, height = 24)
-pn2 <- fullplot(400, model="ziln", stop.if.absent = FALSE)
-ggsave(file="figure/ziln_full_size400.pdf", pn2, width = 30, height = 24)
-
-
-p1 <- fullplot(80, model="zinb", stop.if.absent = FALSE)
-ggsave(file="figure/zinb_full_size80.pdf", p1, width = 30, height = 24)
-p2 <- fullplot(400, model="zinb", stop.if.absent = FALSE)
-ggsave(file="figure/zinb_full_size400.pdf", p2, width = 30, height = 24)
-
-pp1 <- fullplot(80, model="zig", stop.if.absent = FALSE)
-ggsave(file="figure/zig_full_size80.pdf", pp1, width = 30, height = 24)
-pp2 <- fullplot(400, model="zig", stop.if.absent = FALSE)
-ggsave(file="figure/zig_full_size400.pdf", pp2, width = 30, height = 24)
-
 
 ####power plot####
 
@@ -421,3 +291,135 @@ tmp.p <<- res.points
   p
 }
 powercurve(model = "ziln", stop.if.absent = FALSE)
+
+
+#### Full plot####
+
+fullplot <- function(size, model, res.tmp = TRUE, stop.if.absent = TRUE) { # res.tmp being globally assigned
+  
+  parameter = switch(model, 
+                     zinb = parameterNB, 
+                     zig = parameterLN, 
+                     ziln = parameterLN)
+  k.index = dim(parameter)[1]
+  param.k = apply(parameter[,-1], 1, function(x) paste0("(", paste(x, collapse=", "), ")"))
+  
+  
+  res <- NULL
+  for(i in 1:10)
+  {
+    
+    for(j in 1:5)
+    {
+      cat("\ni = ",i,"j = ",j, "k = ")
+      
+      for(k in 1:k.index)
+      {
+        cat(k , " ")
+        fn.tmp <- paste0("output/stat-n",size,"-pert0.5-",model,"-",i,".",j,".",k,".rds")
+        if (file.exists(fn.tmp)) {
+          result <- readRDS(fn.tmp)
+          result.stat <- data.frame(result$stat)
+          tmp <- 
+            result.stat %>% 
+            mutate ("LB" = LB.glob, "MAST" = MAST.glob, "KW-II" = Wg.glob,
+                    "i" = i,"j" = j,"k" = k,"batch_f" = as.character(result$setting$kappa[4]),
+                    "effect" = as.character(result$setting$delta[4])) %>%
+            dplyr::select(LB, LN, MAST, KW, `KW-II`, DS2, `DS2ZI`, MGS, 
+                          ANCOM.sz, ANCOM, i, j, k, batch_f, effect)
+          
+          res <- rbind(res,tmp[1,])
+          
+        } else {
+          if (stop.if.absent) stop("Not available")
+          cat("(Not available) ")
+        }
+      }
+    }
+  }
+  res <- res %>% gather(key = "method", value = "p.value",`LB`,`LN`,`MAST`,`KW`,
+                        `KW-II`,`DS2`, `DS2ZI`, `MGS`, `ANCOM.sz`, `ANCOM`)
+  
+  res$method_f = factor(res$method,
+                        levels = c("LN", "LB", "MAST", "DS2", "DS2ZI", "MGS", "ANCOM.sz", "ANCOM", "KW", "KW-II"),
+                        labels = c("LN", "LB", "MAST", "DS2", "DS2ZI", "MGS", "ANCOMsz", "ANCOM", "KW", "KW-II"))
+  res$effect_f = factor(res$effect,
+                        levels = c("Effect_null", "Effect_mu(D>H)", 
+                                   "Effect_theta(D>H)", "Effect_pi(D<H)",
+                                   "Effect_mu(D>H).theta(D>H)", "Effect_mu(D>H).pi(D<H)", 
+                                   "Effect_theta(D>H).pi(D<H)", "Effect_mu(D>H),pi(D>H)",
+                                   "Effect_mu(D>H).theta(D<H)", "Effect_theta(D<H).pi(D<H)"), 
+                        labels = c(TeX("D1 (null)"),
+                                   TeX("D2 ($\\mu_D$>$\\mu_{H}$)"), 
+                                   TeX("D3 ($\\theta_D$>$\\theta_{H}$)"),
+                                   TeX("D4 ($\\pi_D$<$\\pi_{H}$)"),
+                                   TeX("D5 ($\\mu_D$>$\\mu_H$, $\\theta_D$>$\\theta_{H}$)"),
+                                   TeX("D6 ($\\mu_D$>$\\mu_H$, $\\pi_D$<$\\pi_{H}$)"),
+                                   TeX("D7 ($\\theta_D$>$\\theta_H$, $\\pi_D$<$\\pi_{H}$)"),
+                                   TeX("D8 ($\\mu_D$>$\\mu_H$, $\\pi_D$>$\\pi_{H}$)"),
+                                   TeX("D9 ($\\mu_D$>$\\mu_H$, $\\theta_D$<$\\theta_{H}$)"),
+                                   TeX("D10 ($\\theta_D$<$\\theta_H$, $\\pi_D$<$\\pi_{H}$)")))
+  res$batch_f = factor(res$batch_f,
+                       levels = c("no batch effect", 
+                                  "small(+,+,-) batch effect",
+                                  "large(+,+,-) batch effect",
+                                  "large(+,-,-) batch effect",
+                                  "small(+,-,-) batch effect"),
+                       labels = c("K1 (0, 0, 0)",
+                                  "K2 (0.5, 0.5, -0.5)",
+                                  "K3 (1, 1, -1)",
+                                  "K4 (0.5, -0.5, -0.5)",
+                                  "K5 (1, -1, -1)"))
+  # res.tmp <<- res
+  # res[res$method %in% c("MGS", "ANCOM", "ANCOM.sz") 
+  #     & res$j != 1, "p.value"] <- NA #NA for MGS, ANCOM, and ANCOM.sz with batch effects
+  res$k <- factor(res$k)
+  if (res.tmp) res.tmp <<- res
+  res %>%
+    ggplot(aes(k, p.value, fill = batch_f)) +
+    geom_bar(stat="identity", position = position_dodge(width = .8)) +
+    geom_hline(yintercept=0.05, col="black", linetype = 2) + ylim(c(0, 1)) + 
+    geom_point(aes(col = batch_f), position = position_dodge(width = .8), shape = 15, size = 0.1) +
+    # geom_text(data = res %>% filter(p.value==0), aes(label = p.value, col = batch_f), 
+    #           position = position_dodge(width = .8), size = 0.5) +
+    # geom_point(data = res %>% filter(p.value==0), aes(k, p.value, fill = batch_f, col = batch_f),
+    #            shape = 21, size = 0.2,
+    #            position = position_dodge()) +
+    guides(col = FALSE) + 
+    theme(legend.position = "none", axis.text.x = element_text(angle=90)) +
+    scale_x_discrete(labels=param.k,guide = guide_axis(n.dodge = 2)) +
+    scale_color_manual(values=c("K1 (0, 0, 0)"  = "dodgerblue",
+                                "K2 (0.5, 0.5, -0.5)"  = "darkgreen",
+                                "K3 (1, 1, -1)" = "chartreuse3",
+                                "K4 (0.5, -0.5, -0.5)"  = "red3",
+                                "K5 (1, -1, -1)" = "tomato1")) +
+    scale_fill_manual(name = TeX("Batch effects ($\\kappa_\\mu, \\kappa_\\theta, \\kappa_{\\pi}$)"),
+                      values=c("K1 (0, 0, 0)"  = "dodgerblue",
+                               "K2 (0.5, 0.5, -0.5)"  = "darkgreen",
+                               "K3 (1, 1, -1)" = "chartreuse3",
+                               "K4 (0.5, -0.5, -0.5)"  = "red3",
+                               "K5 (1, -1, -1)" = "tomato1")) +
+    xlab(expression("baseline (" * mu ~ ", " * theta * ", " * pi * ")")) +
+    ylab("rejection rate") +
+    facet_grid(rows = vars(method_f), cols = vars(effect_f), labeller = label_parsed) +
+    theme(plot.title = element_text(hjust = 0.5), legend.position="bottom")  -> p
+  
+  p
+}
+
+#width = 32, height = 16
+pn1 <- fullplot(80, model="ziln", stop.if.absent = FALSE)
+ggsave(file="figure/ziln_full_size80.pdf", pn1, width = 30, height = 24)
+pn2 <- fullplot(400, model="ziln", stop.if.absent = FALSE)
+ggsave(file="figure/ziln_full_size400.pdf", pn2, width = 30, height = 24)
+
+
+p1 <- fullplot(80, model="zinb", stop.if.absent = FALSE)
+ggsave(file="figure/zinb_full_size80.pdf", p1, width = 30, height = 24)
+p2 <- fullplot(400, model="zinb", stop.if.absent = FALSE)
+ggsave(file="figure/zinb_full_size400.pdf", p2, width = 30, height = 24)
+
+pp1 <- fullplot(80, model="zig", stop.if.absent = FALSE)
+ggsave(file="figure/zig_full_size80.pdf", pp1, width = 30, height = 24)
+pp2 <- fullplot(400, model="zig", stop.if.absent = FALSE)
+ggsave(file="figure/zig_full_size400.pdf", pp2, width = 30, height = 24)
