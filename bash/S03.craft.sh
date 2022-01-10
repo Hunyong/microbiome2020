848b93528086f673634fde4481749ea17f69d6f3
hunyong <hunyong.cho@gmail.com>
2022-01-10 13:33:57 -0500

    .

--- bash/S03.craft.sh
+++ bash/S03.craft.sh
@@ -7,14 +7,14 @@ for zoe in 1 2 3; do # ZILN
         echo "Not available.";      
       elif [[ "$ttype" == "bact" ]]; then
         # For ZOE bact, n.gene = 300, n.signal = 30.
-        for sim in 1..10; do
+        for sim in {1..10}; do
           sbatch --time=10:00:00 --mem 10000 bash/runR.sh C02.11.simulation-craft.R  $zoe $j $ttype $sim 30 300 1
         done;
       else
         # For ZOE gene and genebact, different n.gene and n.signal.
         for ngene in 10000 100000; do
           for nsignal in 100 300 1000; do
-            for sim in 1..10; do
+            for sim in {1..10}; do
               sbatch --time=16:00:00 --mem 10000 bash/runR.sh C02.11.simulation-craft.R  $zoe $j $ttype $sim $nsignal $ngene 1
             done;
           done;
